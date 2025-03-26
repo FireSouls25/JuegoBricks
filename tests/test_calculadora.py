342aba699efe35c08a9e5233c79504d2d55a52fb
FireSouls25 <david.castillofl@campusucc.edu.co>
2025-03-26 02:46:34 +0000

    test_calculadora

---
+++ tests/test_calculadora.py
@@ -0,0 +1,18 @@
+import unittest
+from calculadora import suma
+
+class TestSuma(unittest.TestCase):
+    def test_suma_positivos(self):
+        self.assertEqual(suma(2, 3), 5)
+    
+    def test_suma_negativos(self):
+        self.assertEqual(suma(-2, -3), -5)
+    
+    def test_suma_mixto(self):
+        self.assertEqual(suma(-2, 3), 1)
+    
+    def test_suma_cero(self):
+        self.assertEqual(suma(0, 0), 0)
+    
+if __name__ == "__main__":
+    unittest.main()
